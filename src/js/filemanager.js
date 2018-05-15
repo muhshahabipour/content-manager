bf31da80348563989241595d95287db6e973d8c3
shahabi <shahabi@vasl.ir>
2018-05-15 14:03:16 +0430

    update

--- src/js/filemanager.js
+++ src/js/filemanager.js
@@ -10,14 +10,17 @@ export default class fileManager {
     init = (button, coreClass) => {
         var fileItems = document.querySelectorAll(".item-fm-file");
         let $modal = this.modal;
+        console.log("H01")
+        console.log("H02", coreClass)
         fileItems.forEach((item) => {
             item.addEventListener('click', (event) => {
                 
                 // console.log("address", event.target.dataset.address);
                 // console.log("sectionId", button.data("sectionId"));
-
+                console.log("H03", coreClass)
                 $('#cm-content-' + button.data("sectionId")).html(event.target.dataset.address);
                 coreClass.updateContentText(document.getElementById('#cm-content-' + button.data("sectionId")));
+                console.log("H04", coreClass)
                 $modal.modal("hide");
             })
         })
