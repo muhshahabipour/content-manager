3f3e44ad8be432feb34dc5c42132b8897da5779d
shahabi <shahabi@vasl.ir>
2018-05-15 10:50:45 +0430

    update

--- src/js/core.js
+++ src/js/core.js
@@ -147,6 +147,61 @@ export default class core {
             thisClass.updateContentText(contenteditableDiv);
         });
 
+
+        $('#fileManagerModal').on('show.bs.modal', function (event) {
+            var button = $(event.relatedTarget);
+
+            console.log(button.data());
+            console.log("target", event.target);
+            console.log("currentTarget", event.currentTarget);
+            console.log("relatedTarget", event.relatedTarget);
+
+            var modal = $(this)
+            const filemanager = new fileManager(modal);
+
+            // TODO: Optimize File/Folder List
+            $.ajax({
+                    url: defaults.ajax.url,
+                    method: defaults.ajax.method,
+
+                    data: extend({
+                        nextPagekey: '',
+                        path: '/'
+                    }, defaults.ajax.data),
+                    headers: defaults.ajax.headers
+                })
+                .then(function (response) {
+                    console.info(response);
+                    if (response.status === 1) {
+
+                        response.directoryInfo.data.forEach((item) => {
+                            if (item.isDirectory) {
+                                // modal.find('.modal-body .fm-wrapper').append(fileManagerItemFolder({name: item.name}));
+                            } else {
+                                modal.find('.modal-body .fm-wrapper').append(fileManagerItemFile({
+                                    name: item.name,
+                                    path: item.linkHost + item.linkPath
+                                }));
+                            }
+
+                        });
+
+                        filemanager.init(button);
+                    }
+
+                })
+                .catch(function (error) {
+                    console.error(error);
+                });
+
+        })
+
+
+        $('#fileManagerModal').on('hide.bs.modal', function (event) {
+            var modal = $(this)
+            modal.find('.modal-body .fm-wrapper').html("");
+        })
+
         /************************ end event listener ************************/
 
 
