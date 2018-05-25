e12d90b07cf402dd59d1d3800718ff2d6247c85c
muh.shahabipour <shahabi@>
2018-05-25 15:45:00 +0430

    update

--- src/js/filemanager.js
+++ src/js/filemanager.js
@@ -12,10 +12,14 @@ export default class fileManager {
             item.addEventListener('click', (event) => {
                 // console.log("address", event.target.dataset.address);
                 // console.log("sectionId", button.data("sectionId"));
-                $('#cm-content-' + button.data("sectionId")).html(event.target.dataset.address);
-                coreClass.updateContentObject(document.getElementById('cm-content-' + button.data("sectionId")), button.data("type"));
-                let buttonCtrl = document.querySelector('#cm-btn-control-' + button.data("sectionId"));
-                buttonCtrl.classList.add("hidden");
+                if (!button.data("isExtra")) {
+                    $('#cm-content-' + button.data("sectionId")).html(event.target.dataset.address);
+                    coreClass.updateContentObject(document.getElementById('cm-content-' + button.data("sectionId")), button.data("type"));
+                    let buttonCtrl = document.querySelector('#cm-btn-control-' + button.data("sectionId"));
+                    buttonCtrl.classList.add("hidden");
+                }else{
+                    $('#' + button.id + ' + input').val(event.target.dataset.address)
+                }
                 $modal.modal("hide");
             })
         })
