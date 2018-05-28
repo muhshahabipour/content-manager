bd0829ab51d00caa611b705b5e3c9adff938693b
shahabi <shahabi@vasl.ir>
2018-05-28 17:25:22 +0430

    update

--- src/js/link.js
+++ src/js/link.js
@@ -1,4 +1,8 @@
+import general from './general-functions'
+
+
 const urlRegExp = new RegExp("^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$");
+
 export default class link {
     modal = null;
 
@@ -12,6 +16,8 @@ export default class link {
 
         submit.addEventListener('click', (event) => {
             const link = document.querySelector("#link");
+
+            // TODO: test "dsafsgg" not okey
             if (link.value && (link.value).match(urlRegExp)) {
                 document.querySelector('#cm-content-' + button.data("sectionId")).innerHTML = link.value;
                 coreClass.updateContentObject(document.getElementById('cm-content-' + button.data("sectionId")), button.data("type"));
