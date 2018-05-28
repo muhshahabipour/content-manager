bb3f6e6e1c9b47cf5ed37898165c86697c11dd6f
shahabi <shahabi@vasl.ir>
2018-05-28 17:13:21 +0430

    update

--- src/js/link.js
+++ src/js/link.js
@@ -15,6 +15,11 @@ export default class link {
             if (link.value && (link.value).match(urlRegExp)) {
                 document.querySelector('#cm-content-' + button.data("sectionId")).innerHTML = link.value;
                 coreClass.updateContentObject(document.getElementById('cm-content-' + button.data("sectionId")), button.data("type"));
+
+                coreClass.createSection(document.querySelector("#cm-section-" + button.data("sectionId")));
+
+                general.setEndOfContenteditable(document.querySelector(".cm-wrapper").lastElementChild.querySelector('.cm-content'));
+
                 $modal.modal("hide");
             } else {
                 if (!link.value) {
