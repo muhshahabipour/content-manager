2ac77226f1242f542a8ba596906625a88c219fe6
shahabi <shahabi@vasl.ir>
2018-06-23 12:07:21 +0430

    update

--- src/js/core.js
+++ src/js/core.js
@@ -57,6 +57,7 @@ export default class core {
 
         new FileManager({
             target: this.defaults.target,
+            modalId: 'contentFileManagerModal',
             ajax: this.defaults.ajax
         });
 
