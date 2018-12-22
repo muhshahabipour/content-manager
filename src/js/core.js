0574bec11df5c6a82c9d3c88bd4d840116c52da8
hamed <h.khoshpasand@gmail.com>
2018-12-22 11:04:58 +0330

    22Dec

--- src/js/core.js
+++ src/js/core.js
@@ -259,11 +259,9 @@ export default class core {
                     // remove section
                     event.preventDefault();
 
-                    if (self.elem.querySelectorAll('.cm-section').length > 1 && (self.data).findIndex((item)=>{ return item.id == id}) > 0) {
-                      
-
+                    if (self.elem.querySelectorAll('.cm-section').length > 1 && (self.data).findIndex((item)=>{ return item.id == id}) > -1) {
                         self.removeDataItem(contenteditableDiv);
-                        general.setEndOfContenteditable(section.previousSibling.querySelector('.cm-content'));
+                        general.setEndOfContenteditable(section.querySelector('.cm-content'));
                         self.elem.removeChild(section);
                     }
                 }
